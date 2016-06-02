b9795e0d2d07709114a5602772c873d2c876ad44
mpapas <mpapasevastos@hotmail.com>
2016-06-01 20:46:15 -0500

    Added MVC project

---
+++ GetReady.Client.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetReady.Client.Mvc.Startup))]
+namespace GetReady.Client.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
